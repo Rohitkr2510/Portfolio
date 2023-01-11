30cfbef69d6c35464970b890126fc3a4d5c70fba
Rohitkr2510 <rohit70_soe@jnu.ac.in>
2023-01-12 01:34:13 +0530

    first commmit

---
+++ src/components/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Header() {
+  return (
+    
+    <nav>
+        <NavContent/>
+    </nav>
+  )
+}
+
+const NavContent = () => {
+    return (
+    <>
+    <h2>Rohit.</h2>
+    <div>
+        <a href='#home'>Home</a>
+        <a href='#work'>Work</a>
+        <a href='#experience'>Experience</a>
+        <a href='#services'>Services</a>
+        <a href='#testimonial'>Testimonial</a>
+        <a href='#contact'>Contact</a>
+    </div>
+    <a href='mailto:rohit70_soe@jnu.ac.in'>
+        <button>Email</button>
+    </a>
+
+    </>
+    )
+}
+
+export default Header
---
+++ src/components/Home.jsx
@@ -0,0 +1,105 @@
+import React, { useRef } from "react";
+import { animate, motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
+import { BsArrowUpRight, BsChevronDown } from "react-icons/bs";
+import me from "../assets/logo1.png";
+
+function Home() {
+
+    const clientCount = useRef(null);
+    const projectCount = useRef(null);
+
+    const animationClientsCount = () => {
+        animate(0, 100, {
+            duration: 1,
+            onUpdate: (v) => (clientCount.current.textContent = v.toFixed()), 
+        })
+    }
+
+    const projectClientsCount = () => {
+        animate(0, 500, {
+            duration: 1,
+            onUpdate: (v) => (projectCount.current.textContent = v.toFixed()), 
+        })
+    }
+
+
+  const animation = {
+    h1: {
+      initial: {
+        x: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        x: 0,
+        opacity: 1,
+      },
+    },
+    button: {
+      initial: {
+        y: "-100%",
+        opacity: 0,
+      },
+      whileInView: {
+        y: 0,
+        opacity: 1,
+      },
+    },
+  };
+  return (
+    <div id="home">
+      <section>
+        <div>
+          <motion.h1 {...animation.h1}>
+            Hi, I Am <br /> Rohit Kumar Mahato
+          </motion.h1>
+
+          <Typewriter
+            options={{
+              strings: ["A Developer", "A Desiger", "A Creator"],
+              autoStart: true,
+              loop: true,
+              cursor: "",
+              wrapperClassName: "typewriterpara",
+            }}
+          />
+
+          <div>
+            <a href="mailto:rohit70_soe@jnu.ac.in">Hire me</a>
+            <a href="#work">
+              Projects <BsArrowUpRight />
+            </a>
+          </div>
+
+          <article>
+            <p>
+              +<motion.span ref = {clientCount} whileInView = {animationClientsCount}></motion.span>
+            </p>
+            <span>Clients Worldwide</span>
+          </article>
+
+          <aside>
+            <article>
+              <p>
+              +<motion.span ref = {projectCount} whileInView = {projectClientsCount}></motion.span>
+
+              </p>
+              <span>Projects Done</span>
+            </article>
+
+            <article data-special>
+              <p>Contact</p>
+              <span>rohit70_soe@jnu.ac.in</span>
+            </article>
+          </aside>
+        </div>
+      </section>
+      <section>
+        <img src={me} alt="Rohit" />
+      </section>
+      <BsChevronDown />
+    </div>
+  );
+}
+
+export default Home;
